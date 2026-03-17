2a368accdd768da3e12f89cf45792ed8345b327a
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-17 19:00:04 -0400

    newcode

--- src/main/java/Bicycle.java
+++ src/main/java/Bicycle.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Bicycle {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+
+
+        System.out.print("Enter age: ");
+        String age = scanner.nextLine();
+
+        int number = 5;
+
+        System.out.println("I am " + number + "years old");
+        System.out.printf("My name is %s, I am %s years old", name, age);
     }
     }
 
