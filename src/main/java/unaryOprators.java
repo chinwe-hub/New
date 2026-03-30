a15d075239627a54c7a9977d4019f00de0e4a8ef
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-30 11:37:26 -0400

    unary

--- src/main/java/unaryOprators.java
+++ src/main/java/unaryOprators.java
@@ -1,5 +1,19 @@
 public class unaryOprators {
     public static void main(String[] args) {
+//        int a = 5;
+//        System.out.println(a);
+//        int b = a++;
+//        String h = "done";
+////        System.out.println("The first number is: " + a + " and the second number is: " + b);
+//        System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
+
+
+        int a = 5;
+        System.out.println(a);
+        int b = ++a;
+        String h = "done";
+//        System.out.println("The first number is: " + a + " and the second number is: " + b);
+        System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
 
     }
 }
