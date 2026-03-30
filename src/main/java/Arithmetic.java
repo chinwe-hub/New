a686af442d562fc476028403f687bc174f9d6f72
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-30 10:17:05 -0400

    boolean

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -1,2 +1,12 @@
 public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+    int result = a + b;
+        System.out.println(result);
+
+        boolean answer = a == b;
+        System.out.println(answer);
+    }
 }
