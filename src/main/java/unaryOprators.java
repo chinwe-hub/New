c287ae6779960a95bc2d60eacb9ad72dfca2358f
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-30 14:37:29 -0400

    unary

--- src/main/java/unaryOprators.java
+++ src/main/java/unaryOprators.java
@@ -8,9 +8,23 @@ public class unaryOprators {
 //        System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
 
 
+//        int a = 5;
+//        System.out.println(a);
+//        int b = ++a;
+//        String h = "done";
+////        System.out.println("The first number is: " + a + " and the second number is: " + b);
+//        System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
+
+
+//        int a = 5;
+//        int b = a--;
+//        String h = "done";
+////        System.out.println("The first number is: " + a + " and the second number is: " + b);
+//        System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
+
+
         int a = 5;
-        System.out.println(a);
-        int b = ++a;
+        int b = --a;
         String h = "done";
 //        System.out.println("The first number is: " + a + " and the second number is: " + b);
         System.out.printf("The first number is %d and the second number is %d, so it is %s", a, b, h);
