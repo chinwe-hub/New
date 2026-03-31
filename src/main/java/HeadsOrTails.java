031d42d0d1926538bbec92e6f3124893938a938d
Chinwe <chinwenduohabuire16@gmail.com>
2026-03-31 10:54:25 -0400

    heads

---
+++ src/main/java/HeadsOrTails.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HeadsOrTails {
+    public static void main(String[] args) {
+//        isheads = game
+        String correct = "head";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side of coin: ");
+        String side = scanner.nextLine();
+
+        if (side.equals(correct)){
+            System.out.println("Let's go to the game");
+        } else {
+            System.out.println("Let's watch a movie");
+        }
+
+    }
+}
